da56ff7150c44c8910e0df2c1143dc53c4981cef
Chandima Ranaweera <chan4lk@hotmail.com>
2015-04-01 18:29:45 +0000

    nuget cleaning
    git-tfs-id: [https://uximagine.visualstudio.com/DefaultCollection]$/Magicurve;C61

---
+++ Uximagine/Magicurve/DEV/SourceCode/Main/Src/ImageProcessing/Uximagine.Magicurve.Image.Processing/ShapeCheckers/FiveCornerShapeChecker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uximagine.Magicurve.Image.Processing.ShapeCheckers
+{
+    public class FiveCornerShapeChecker
+    {
+
+    }
+}
--- Uximagine/Magicurve/DEV/SourceCode/Main/Src/Services/Uximagine.Magicurve.Services.BusinessServices/Class1.cs
+++ Uximagine/Magicurve/DEV/SourceCode/Main/Src/Services/Uximagine.Magicurve.Services.BusinessServices/Class1.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Uximagine.Magicurve.Services.BusinessServices
+﻿namespace Uximagine.Magicurve.Services.BusinessServices
 {
     public class Class1
     {
