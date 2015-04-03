9fa3af52f002aaf2790478aa94ece867294bb5b2
Nadeera Kodithuwakku <kodithuwakku.uom@outlook.com>
2015-04-03 12:25:26 +0000

    modified home zebra
    git-tfs-id: [https://uximagine.visualstudio.com/DefaultCollection]$/Magicurve;C73

--- Uximagine/Magicurve/DEV/SourceCode/Main/Src/UI/Uximagine.Magicurve.UI.Web/Scripts/app/views/home/home.zebra.js
+++ Uximagine/Magicurve/DEV/SourceCode/Main/Src/UI/Uximagine.Magicurve.UI.Web/Scripts/app/views/home/home.zebra.js
@@ -2,41 +2,104 @@
     eval(zebra.Import("ui", "layout"));
 
     //create json object array
-    var jsonObj = [{ "controlsName": "Button" }, { "controlsName": "CheckBox" }];
+    var jsonObj = [{ "controlsName": "Button", "width": 50, "height": 60 }, { "controlsName": "CheckBox", "width": 100, "height": 60 }];
 
     var controls = [];
     //controls.length = jsonObj.length;
 
-    
+
 
     //function for drawing checkbox
-    function drawCheckBox() {
+    function drawCheckBox(cwidth,cheight) {
         var checkbox = new zebra.ui.Checkbox("CheckBox");
+        checkbox.properties({
+            width: cwidth,
+            height: cheight,
+        });
         var checkboxShaperPan = new zebra.ui.designer.ShaperPan(checkbox);
         checkboxShaperPan.properties({
             value: true,
             location: [10, 10]
 
         });
+        checkboxShaperPan.extend([
+        function mouseDragged(e) {
+            
+            var dy = (e.absY - this.py),
+                    dx = (e.absX - this.px),
+                    s = this.state,
+                    nw = this.width - dx * s.left + dx * s.right,
+                    nh = this.height - dy * s.top + dy * s.bottom;
+
+            var controls_width = nw - 14;
+            var controls_height = nh - 14;
+
+            var width_differ = Math.abs(this.width-nw);
+            var height_differ = Math.abs(this.height - nh);
+            
+            var newXlocation = (this.x + dx * s.left)+7;
+            var newYlocation = (this.y + dy * s.top) + 7;
+
+            if (dx != 0 || dy != 0) {
+                console.log("shaper xlocation" + " " + (this.x + dx * s.left) + " " + "shaper ylocation" + " " + (this.y + dy * s.top));
+                console.log("controller xlocation" + " " + newXlocation + " " + "controller ylocation" + " " + newYlocation);
+            } else {
+                console.log("Location Not Changed");
+            }
+
+            if (width_differ != 0 || height_differ != 0) {
+                console.log("shaper width" + " " + nw + " " + "shaper height" + " " + nh);
+                console.log("controller width" + " " + controls_width + " " + "controller height" + " " + controls_height);
+            } else {
+                 console.log("Size Not Changed");
+
+            }
+            //console.log("dy " + dy);
+            //console.log("e.absY " + e.absY);
+            //console.log("this.py " + this.py);
+            //console.log("dx " + dx);
+            //console.log("e.absX " + e.absX);
+            //console.log("this.px " + this.px);
+            //console.log("state " + s);
+            //console.log("state left " + s.left);
+
+            this.$super(e);;
+        }
+
+        ]);
         return checkboxShaperPan;
 
     }
 
 
     //function for drawing button
-    function drawButton() {
+    function drawButton(bwidth,bheight) {
         var button = new zebra.ui.Button("Button");
+        button.properties({
+            width: bwidth,
+            height: bheight
+
+        });
         var buttonShaperPan = new zebra.ui.designer.ShaperPan(button);
         buttonShaperPan.properties({
             value: true,
-            location: [90,50]
+            location: [90, 50]
 
         });
 
         buttonShaperPan.extend([
         function mouseDragged(e) {
             this.$super(e);
-            console.log(e);
+
+            var dy = (e.absY - this.py),
+                    dx = (e.absX - this.px),
+                    s = this.state,
+                    nw = this.width - dx * s.left + dx * s.right,
+                    nh = this.height - dy * s.top + dy * s.bottom;
+            //console.log(nw + " " + nh);
+            //var buttonWidth = e.kids.width;
+            //console.log(buttonWidth);
+            //console.log(e);
         }
 
         ]);
@@ -45,32 +108,36 @@
 
     }
 
+    function drawComboBox() {
+        var combo = new zebra.ui.Combo(new zebra.ui.List([
+       "Item 1",
+       "Item 2",
+       "Item 3"
+        ]));
+
+    }
+
     var controlsValid = 0;
 
     for (var i = 0; i < jsonObj.length; i++) {
         if (jsonObj[i].controlsName == "Button") {
-            controls[controlsValid] = drawButton();
+            controls[controlsValid] = drawButton(jsonObj[i].width,jsonObj[i].height);
             controlsValid++;
-        } else if(jsonObj[i].controlsName == "CheckBox"){
-            controls[controlsValid] = drawCheckBox();
+        } else if (jsonObj[i].controlsName == "CheckBox") {
+            controls[controlsValid] = drawCheckBox(jsonObj[i].width, jsonObj[i].height);
             controlsValid++;
         }
     }
 
-    //function assignKids(){
 
-        
-
-    //}
-    
 
 
     var root = (new zCanvas("designer", 1000, 400)).root;
     //var p = new zebra.ui.Button("Test");
     //var testShaperPan = new zebra.ui.designer.ShaperPan(p);
     //var drawCheckBox = drawCheckBox();
-    var drawButton = drawButton();
-  
+   // var drawButton = drawButton();
+
     root.properties({
         layout: new BorderLayout(4, 4),
         border: new Border(),
@@ -78,33 +145,7 @@
         kids: {
             CENTER: new BorderPan("Designer panel", new Panel({
                 padding: 6,
-                kids: controls//[
-
-                    //drawCheckBox,
-
-                    //testShaperPan.properties({
-                    //    value: true,
-                    //    location: [10, 50]
-
-                    //}),
-
-                    //drawButton,
-
-                    //new zebra.ui.designer.ShaperPan(new Button("Button").properties({
-                    //    value: true,
-                    //    location: [90, 50]
-                    //})
-
-                        //).properties({
-                        //    isResizeEnabled: true
-                        //}),
-
-                //    new zebra.ui.designer.ShaperPan(new Slider().properties({
-                //        value: true,
-                //        size: [120, 60],
-                //        location: [30, 100]
-                //    }))
-                //]
+                kids: controls
             })),
 
             BOTTOM: new Button("Align", [
@@ -122,5 +163,5 @@
     });
 
 
-    
+
 });
