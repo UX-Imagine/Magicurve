daa4714c4801aba9d89beeb2e91d24a0b09e64e7
Nadeera Kodithuwakku <kodithuwakku.uom@outlook.com>
2015-04-03 08:47:16 +0000

    modified home.zebra js with controls
    git-tfs-id: [https://uximagine.visualstudio.com/DefaultCollection]$/Magicurve;C70

--- Uximagine/Magicurve/DEV/SourceCode/Main/Src/UI/Uximagine.Magicurve.UI.Web/Scripts/app/views/home/home.zebra.js
+++ Uximagine/Magicurve/DEV/SourceCode/Main/Src/UI/Uximagine.Magicurve.UI.Web/Scripts/app/views/home/home.zebra.js
@@ -1,7 +1,76 @@
 ﻿zebra.ready(function () {
     eval(zebra.Import("ui", "layout"));
 
-    var root = (new zCanvas("designer", 300, 300)).root;
+    //create json object array
+    var jsonObj = [{ "controlsName": "Button" }, { "controlsName": "CheckBox" }];
+
+    var controls = [];
+    //controls.length = jsonObj.length;
+
+    
+
+    //function for drawing checkbox
+    function drawCheckBox() {
+        var checkbox = new zebra.ui.Checkbox("CheckBox");
+        var checkboxShaperPan = new zebra.ui.designer.ShaperPan(checkbox);
+        checkboxShaperPan.properties({
+            value: true,
+            location: [10, 10]
+
+        });
+        return checkboxShaperPan;
+
+    }
+
+
+    //function for drawing button
+    function drawButton() {
+        var button = new zebra.ui.Button("Button");
+        var buttonShaperPan = new zebra.ui.designer.ShaperPan(button);
+        buttonShaperPan.properties({
+            value: true,
+            location: [90,50]
+
+        });
+
+        buttonShaperPan.extend([
+        function mouseDragged(e) {
+            this.$super(e);
+            console.log(e);
+        }
+
+        ]);
+
+        return buttonShaperPan;
+
+    }
+
+    var controlsValid = 0;
+
+    for (var i = 0; i < jsonObj.length; i++) {
+        if (jsonObj[i].controlsName == "Button") {
+            controls[controlsValid] = drawButton();
+            controlsValid++;
+        } else if(jsonObj[i].controlsName == "CheckBox"){
+            controls[controlsValid] = drawCheckBox();
+            controlsValid++;
+        }
+    }
+
+    //function assignKids(){
+
+        
+
+    //}
+    
+
+
+    var root = (new zCanvas("designer", 1000, 400)).root;
+    //var p = new zebra.ui.Button("Test");
+    //var testShaperPan = new zebra.ui.designer.ShaperPan(p);
+    //var drawCheckBox = drawCheckBox();
+    var drawButton = drawButton();
+  
     root.properties({
         layout: new BorderLayout(4, 4),
         border: new Border(),
@@ -9,23 +78,33 @@
         kids: {
             CENTER: new BorderPan("Designer panel", new Panel({
                 padding: 6,
-                kids: [
-                    new zebra.ui.designer.ShaperPan(new Checkbox("Check-box").properties({
-                        value: true,
-                        location: [10, 10]
-                    })),
+                kids: controls//[
 
-                    new zebra.ui.designer.ShaperPan(new Button("Button").properties({
-                        value: true,
-                        location: [90, 50]
-                    })),
+                    //drawCheckBox,
 
-                    new zebra.ui.designer.ShaperPan(new Slider().properties({
-                        value: true,
-                        size: [120, 60],
-                        location: [30, 100]
-                    }))
-                ]
+                    //testShaperPan.properties({
+                    //    value: true,
+                    //    location: [10, 50]
+
+                    //}),
+
+                    //drawButton,
+
+                    //new zebra.ui.designer.ShaperPan(new Button("Button").properties({
+                    //    value: true,
+                    //    location: [90, 50]
+                    //})
+
+                        //).properties({
+                        //    isResizeEnabled: true
+                        //}),
+
+                //    new zebra.ui.designer.ShaperPan(new Slider().properties({
+                //        value: true,
+                //        size: [120, 60],
+                //        location: [30, 100]
+                //    }))
+                //]
             })),
 
             BOTTOM: new Button("Align", [
@@ -41,4 +120,7 @@
             ])
         }
     });
+
+
+    
 });
