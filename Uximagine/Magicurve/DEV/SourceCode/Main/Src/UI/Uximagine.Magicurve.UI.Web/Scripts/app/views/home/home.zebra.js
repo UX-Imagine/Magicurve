60fbcfe187a3ea12efb6cf0836af0975691e45f5
Dismika Harishchandra <dismika@hotmail.com>
2015-11-20 09:23:09 +0000

    git-tfs-id: [https://uximagine.visualstudio.com/DefaultCollection]$/Magicurve;C127

--- Uximagine/Magicurve/DEV/SourceCode/Main/Src/UI/Uximagine.Magicurve.UI.Web/Scripts/app/views/home/home.zebra.js
+++ Uximagine/Magicurve/DEV/SourceCode/Main/Src/UI/Uximagine.Magicurve.UI.Web/Scripts/app/views/home/home.zebra.js
@@ -2,7 +2,7 @@
     // build Zebra canvas component that adds new
     // Canvas DOM element into page with the given size
     var zCanvas = new zebra.ui.zCanvas("designer", 1300, 1300);
-    zCanvas.setBackground("red");
+    zCanvas.setBackground("black");
     var root = zCanvas.root; // save reference to root UI component
 
   //  var root = (new zCanvas("designer", 400, 300)).root;
@@ -16,9 +16,18 @@
     //              +--- zebra.ui.TextField  
     var p = new zebra.ui.Panel(); // create panel
     p.setBounds(10, 10, 380, 280); // shape panel
-    p.setBackground("yellow");    // set yellow background
+    p.setBackground("green");    // set yellow background
     root.add(p);                  // add panel to root
-   
+
+    var d = new zebra.ui.Panel(); // create panel
+    d.setBounds(400, 10, 600, 280); // shape panel
+    d.setBackground("red");    // set yellow background
+    root.add(d);                  // add panel to root
+
+    var b = new zebra.ui.Button("Test Button"); // create button
+    b.setBounds(10, 60, 360, 50); // shape button
+  //  d.add(b);                     // add the button as a kid to panel
+
     var l = new zebra.ui.Label("Test Label"); // create label
     l.setBounds(10, 10, 360, 50); // shape label
     p.add(l);                     // add the label as a kid to panel
@@ -34,8 +43,49 @@
 
     var tf = new zebra.ui.TextField("Test Text Field");
     tf.setBounds(30, 10, 200, 30);
-    
+   
     pp.add(tf);
 
+    var b = new zebra.ui.Checkbox("check");
+    b.setBounds(10, 60, 360, 50); // shape button
+    d.add(b);
+
+    var b = new zebra.ui.Radiobox("radio");
+    b.setBounds(80, 60, 360, 50); // shape button
+    d.add(b);
+    var b = new zebra.ui.Button("button");
+    
+    b.setBounds(160, 70, 50, 25); // shape button
+    d.add(b);
+
+    var b = new zebra.ui.Combo("Combo");
+    b.setBounds(220, 70, 50, 25); // shape button
+    d.add(b);
+
+    var b = new zebra.ui.HtmlTextArea("TextArea");
+    
+    b.setBounds(275, 70, 150, 25); // shape button
+    d.add(b);
    
+    var b = new zebra.ui.HtmlTextField("HtmlTextField");
+    b.setBounds(10, 100, 150, 25); // shape button
+    d.add(b);
+
+    var b = new zebra.ui.Line("line");
+    b.setLineColors("black");
+    b.setBounds(170, 100, 150, 25); // shape button
+    d.add(b);
+
+    var b = new zebra.ui.MenuItem("menu");
+   
+    b.setBounds(170, 100, 100, 25); // shape button
+    d.add(b);
+
+    var b = new zebra.ui.Progress("PassTextField");
+    b.setBounds(270, 100, 150, 25); // shape button
+    d.add(b);
+    var b = new zebra.ui.Tool("Progress");
+    b.setBounds(10, 100, 150, 25); // shape button10
+    d.add(b);
 });
+
