ec68072888b54c59aa90db855a15c88a24d61143
Dismika Harishchandra <dismika@hotmail.com>
2015-11-23 09:16:22 +0000

    new edit
    git-tfs-id: [https://uximagine.visualstudio.com/DefaultCollection]$/Magicurve;C146

--- Uximagine/Magicurve/DEV/SourceCode/Main/Src/UI/Uximagine.Magicurve.UI.Web/Scripts/app/views/home/home.Index.js
+++ Uximagine/Magicurve/DEV/SourceCode/Main/Src/UI/Uximagine.Magicurve.UI.Web/Scripts/app/views/home/home.Index.js
@@ -1,6 +1,5 @@
 ﻿zebra.ready(function () {
-    // build Zebra canvas component that adds new
-    // Canvas DOM element into page with the given size
+ 
     var zCanvas = new zebra.ui.zCanvas("designer", 970, 820);
     //zCanvas.setBackground("red");
     var root = zCanvas.root; // save reference to root UI component
@@ -20,6 +19,9 @@
     labaleImage.setBounds(425,65,450,75)
     header.add(labaleImage);
 
+    /*
+    var butto = new drawPanel(300, 50, 60, 30, "red");
+    header.add(butto);*/
 
     /*
     // insert image 
@@ -118,7 +120,11 @@
     lable3.setColor("black");
     sideBar.add(lable3);
 
-    var comboBox = new zebra.ui.Combo();
+    var comboBox = new zebra.ui.Combo([
+        "Srilanka",
+        "USA",
+        "Australia",
+    ]);
     comboBox.setBounds(130, 350, 100, 30);
     sideBar.add(comboBox);
 
@@ -149,5 +155,142 @@
     hyperlink.setBounds(350, 20, 150, 20);
     hyperlink.setColor("#2E64FE");
     footer.add(hyperlink);
-
+    
 });
+
+
+
+
+function genarateDesign() {
+
+   
+}
+
+
+
+//////////////// method drawing non editable ///////////
+
+//function for drawing button
+function drawButton(bX, bY, bwidth, bheight, ButttonCaption) {
+    var button = new zebra.ui.Button(ButttonCaption);
+    button.setBounds(bX, bY, bwidth, bheight);
+    return button;
+
+}
+
+
+//function for drawing button
+function drawPanel(px, py, pw, ph, panelColor) {
+    var panel = new zebra.ui.Panel();
+    panel.setBounds(px, py, pw, ph);
+    panel.setBackground(panelColor);
+
+    return panel;
+
+}
+
+//function for drawing lable
+function drawLable(lx, ly, lw, lh, labaleCaption) {
+    var lable = new zebra.ui.Label(labaleCaption);
+    lable.setBounds(lx, ly, lw, lh);
+    lable.setColor("black");
+
+    return lable;
+
+}
+
+//function for drawing text box
+function drawTextBox(tx, ty, tw, th) {
+    var textBox = new zebra.ui.TextField("type here...");
+    textBox.setBounds(tx, ty, tw, th);
+
+    return textBox;
+}
+
+//function for drawing Horizontal Line
+function drawHorizontalLine(hx, hy, hw) {
+
+    var horizontal = new zebra.ui.Line();
+    horizontal.setBounds(hx, hy, hw, 3);
+
+    return horizontal;
+}
+
+//function for drawing checkBox
+function drawCheckBox(checkX, checkY, checkW, checkH, checkValue) {
+
+    var checkBox = new zebra.ui.Checkbox(checkValue);
+    checkBox.setBounds(checkX, checkY, checkW, checkH);
+
+    return checkBox;
+}
+
+//function for drawing radio button
+function drawRadioButton(radioX, radioY, radioW, radioH, radioValue) {
+    var radioButton = new zebra.ui.Radiobox(radioValue);
+    radioButton.setBounds(radioX, radioY, radioW, radioH);
+
+    return radioButton;
+}
+
+//function for drawing combo box
+function drawComboBox(comboX, comboY, comboW, comboH) {
+    var combo = new zebra.ui.Combo(new zebra.ui.List([
+   "Item 1",
+   "Item 2",
+   "Item 3"
+    ]));
+
+    combo.setBounds(comboX, comboY, comboW, comboH);
+    return combo;
+}
+
+//function for drawing TextArea
+function drawTextArea(textAreaX, textAreaY, textAreaW, textAreaH) {
+    var textArea = new zebra.ui.TextArea();
+    textArea.setBounds(textAreaX, textAreaY, textAreaW, textAreaH);
+
+    return textArea;
+}
+
+//function for drawing combo box
+function drawHyperlink(hyX, hyY, hyW, hyH, hyperValue) {
+    var hyperlink = new zebra.ui.Link(hyperValue);
+    hyperlink.setBounds(hyX, hyY, hyW, hyH);
+    hyperlink.setColor("#2E64FE");
+
+    return hyperlink;
+}
+
+//function for drawing Menubar
+function drawMenuBar(menuX, menuY, menuW, menuH) {
+    var menu = new zebra.ui.Menubar({
+        "HOME": {
+            "Subitem 1.1": null,
+            "Subitem 1.2": null,
+            "Subitem 1.3": null
+        },
+        "CONTENT": {
+            "Subitem 2.1": null,
+            "Subitem 2.2": null,
+            "Subitem 2.3": null
+        },
+        "CONTCT": null
+    });
+
+    menu.setBounds(menuX, menuY, menuW, menuH);
+
+    return menu;
+}
+
+//function for drawing image
+function drawImageContent(imageX, imageY, imageW, imageH) {
+    var image = new zebra.ui.ImagePan();
+
+    image.setBounds(imageX, imageY, imageW, imageH);
+
+    return image;
+}
+
+
+///////////////////////////////////////////////////////////////
