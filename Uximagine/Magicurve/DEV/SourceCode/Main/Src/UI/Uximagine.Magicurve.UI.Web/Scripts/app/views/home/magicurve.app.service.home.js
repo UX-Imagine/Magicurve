14ca4ef945d68db1656c6b838582d6ea6b3b86a3
Dismika Harishchandra <dismika@hotmail.com>
2015-11-23 05:34:24 +0000

    include json object
    git-tfs-id: [https://uximagine.visualstudio.com/DefaultCollection]$/Magicurve;C142

--- Uximagine/Magicurve/DEV/SourceCode/Main/Src/UI/Uximagine.Magicurve.UI.Web/Scripts/app/views/home/magicurve.app.service.home.js
+++ Uximagine/Magicurve/DEV/SourceCode/Main/Src/UI/Uximagine.Magicurve.UI.Web/Scripts/app/views/home/magicurve.app.service.home.js
@@ -1,16 +1,109 @@
 ﻿magicurveApp.factory("homeService",
     function($http) {
         return {
-            shape : {
+            shape : {/*
                 type    :   "button",
                 top: 20,
                 left: 30,
                 height: 100,
-                width: 100
-            },
+                width: 100*/
 
+
+                 
+   "Controls": [
+     {
+         "Type": 0,
+         "X": 0,
+         "Y": 0,
+         "Width": 0,
+         "Height": 0,
+         "EdgePoints": null
+     },
+     {
+         "Type": 0,
+         "X": 0,
+         "Y": 0,
+         "Width": 0,
+         "Height": 0,
+         "EdgePoints": null
+     },
+     {
+         "Type": 0,
+         "X": 0,
+         "Y": 0,
+         "Width": 0,
+         "Height": 0,
+         "EdgePoints": null
+     },
+     {
+         "Type": 0,
+         "X": 0,
+         "Y": 0,
+         "Width": 0,
+         "Height": 0,
+         "EdgePoints": null
+     },
+     {
+         "Type": 0,
+         "X": 0,
+         "Y": 0,
+         "Width": 0,
+         "Height": 0,
+         "EdgePoints": null
+     },
+     {
+         "Type": 0,
+         "X": 0,
+         "Y": 0,
+         "Width": 0,
+         "Height": 0,
+         "EdgePoints": null
+     },
+     {
+         "Type": 0,
+         "X": 0,
+         "Y": 0,
+         "Width": 0,
+         "Height": 0,
+         "EdgePoints": null
+     },
+     {
+         "Type": 0,
+         "X": 0,
+         "Y": 0,
+         "Width": 0,
+         "Height": 0,
+         "EdgePoints": null
+     },
+     {
+         "Type": 0,
+         "X": 0,
+         "Y": 0,
+         "Width": 0,
+         "Height": 0,
+         "EdgePoints": null
+     },
+     {
+         "Type": 0,
+         "X": 0,
+         "Y": 0,
+         "Width": 0,
+         "Height": 0,
+         "EdgePoints": null
+     },
+     {
+         "Type": 0,
+         "X": 0,
+         "Y": 0,
+         "Width": 0,
+         "Height": 0,
+         "EdgePoints": null
+     }
+   ],
+            },
+            
             getShapeInfo: function () {
-                var promise = $http.get("/values");
+                var promise = $http.get("Magicurve/api/images/controls");
                 return promise;
             }
         }
