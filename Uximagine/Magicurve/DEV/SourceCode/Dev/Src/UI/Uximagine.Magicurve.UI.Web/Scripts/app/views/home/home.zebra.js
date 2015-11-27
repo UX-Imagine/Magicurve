ec16698422e417e393f297a7c8385b1a96531795
Dismika Harishchandra <dismika@hotmail.com>
2015-11-27 11:08:24 +0000

    editabl button draw
    git-tfs-id: [https://uximagine.visualstudio.com/DefaultCollection]$/Magicurve;C157

--- Uximagine/Magicurve/DEV/SourceCode/Dev/Src/UI/Uximagine.Magicurve.UI.Web/Scripts/app/views/home/home.zebra.js
+++ Uximagine/Magicurve/DEV/SourceCode/Dev/Src/UI/Uximagine.Magicurve.UI.Web/Scripts/app/views/home/home.zebra.js
@@ -7,144 +7,6 @@
     var controls = [];
     //controls.length = jsonObj.length;
 
-    //function for drawing button
-    function drawButton(bwidth, bheight) {
-        var button = new zebra.ui.Button("Button");
-        button.properties({
-            width: bwidth,
-            height: bheight
-
-        });
-        var buttonShaperPan = new zebra.ui.designer.ShaperPan(button);
-        buttonShaperPan.properties({
-            value: true,
-            location: [90, 50]
-
-        });
-
-       
-
-        return buttonShaperPan;
-
-    }
-
-    //function for drawing button
-    function drawPanel(px,py,pw,ph) {
-        var panel = new zebra.ui.Panel();
-        panel.setBounds(px, py, pw, ph);
-        panel.setBackground("red");
-
-        var shperPanel = new zebra.ui.designer.ShaperPan(panel);
-
-        return shperPanel;
-
-    }
-
-    //function for drawing lable
-    function drawLable(lx,ly,lw,lh) {
-        var lable = new zebra.ui.Label("lable");
-        lable.setBounds(lx, ly, lw, lh);
-        lable.setColor("black");
-
-          var shperlable = new zebra.ui.designer.ShaperPan(lable);
-
-          return shperlable;
-
-    }
-
-    //function for drawing text box
-    function drawTextBox(tx,ty,tw,th) {
-        var textBox = new zebra.ui.TextField("type here...");
-        textBox.setBounds(tx, ty, tw, th);
-
-        var shpertextBox = new zebra.ui.designer.ShaperPan(textBox);
-
-        return shpertextBox;
-    }
-
-    //function for drawing Horizontal Line
-    function drawHorizontalLine(hx,hy,hw) {
-
-        var horizontal = new zebra.ui.Line();
-        horizontal.setBounds(hx, hy, hw, 3);
-        var shperhorizontal = new zebra.ui.designer.ShaperPan(horizontal);
-
-        return shperhorizontal;
-    }
-
-    //function for drawing checkBox
-    function drawCheckBox(checkX,checkY,checkW,checkH) {
-
-        var checkBox = new zebra.ui.Checkbox("Java");
-        checkBox.setBounds(checkX, checkY, checkW, checkH);
-        var shpercheckBox = new zebra.ui.designer.ShaperPan(checkBox);
-
-        return shpercheckBox;
-    }
-
-    //function for drawing radio button
-    function drawRadioButton(radioX,radioY,radioW,radioH) {
-        var radioButton = new zebra.ui.Radiobox("radio");
-        radioButton.setBounds(radioX, radioY, radioW, radioH);
-        var shpercheckBox = new zebra.ui.designer.ShaperPan(radioButton);
-
-        return shpercheckBox;
-    }
-
-    //function for drawing combo box
-    function drawComboBox(comboX,comboY,comboW,comboH) {
-        var combo = new zebra.ui.Combo(new zebra.ui.List([
-       "Item 1",
-       "Item 2",
-       "Item 3"
-        ]));
-
-        combo.setBounds(comboX, comboY, comboW, comboH);
-        var shpercombo = new zebra.ui.designer.ShaperPan(combo);
-
-        return shpercombo;
-    }
-
-    //function for drawing TextArea
-    function drawTextArea(textAreaX, textAreaY, textAreaW, textAreaH) {
-        var textArea = new zebra.ui.TextArea();
-        textArea.setBounds(textAreaX, textAreaY, textAreaW, textAreaH);
-        var shpertextArea = new zebra.ui.designer.ShaperPan(textArea);
-
-        return shpertextArea;
-    }
-
-    //function for drawing combo box
-    function drawHyperlink(hyX,hyY,hyW,hyH) {
-        var hyperlink = new zebra.ui.Link("This is a hyperlink");
-        hyperlink.setBounds(hyX, hyY, hyW, hyH);
-        hyperlink.setColor("#2E64FE");
-        var shperhyperlink = new zebra.ui.designer.ShaperPan(hyperlink);
-
-        return shperhyperlink;
-    }
-
-    //function for drawing Menubar
-    function drawMenuBar(menuX, menuY, menuW, menuH) {
-        var menu = new zebra.ui.Menubar({
-            "HOME": {
-                "Subitem 1.1": null,
-                "Subitem 1.2": null,
-                "Subitem 1.3": null
-            },
-            "CONTENT": {
-                "Subitem 2.1": null,
-                "Subitem 2.2": null,
-                "Subitem 2.3": null
-            },
-            "CONTCT": null
-        });
-
-        menu.setBounds(menuX, menuY, menuW, menuH);
-        var shpermenu = new zebra.ui.designer.ShaperPan(menu).properties({value : true});
-
-        return shpermenu;
-    }
 
     var controlsValid = 0;
 
@@ -158,64 +20,104 @@
         }
     }
 
-    var root = (new zCanvas("designer", 1000, 400)).root;
-  
-    var pn = new drawPanel(600,10,50,50);
-    root.add(pn);
-    var butt = new drawButton(50, 50);
-    root.add(butt);
-    
-    var lable1 = new drawLable(0,0,100,50);
-    root.add(lable1);
+    var root = (new zCanvas("designer", 970, 820)).root;
+    root.setBackground("black");
 
-    var text = new drawTextBox(500,80,80,30);
-    root.add(text);
+    var pn1 = new drawPanel(5, 5, 960, 810);
+    pn1.setBackground("white");
+    root.add(pn1);
 
-    var hor = new drawHorizontalLine(0, 330, 250);
-    root.add(hor);
+    var pn2 = new drawPanel(700, 155, 260, 655);
+    pn2.setBackground("#CEF6F5");
+    root.add(pn2);
 
-    var radio = new drawRadioButton(20, 200, 80, 30);
-    root.add(radio);
+    var image1 = new drawImageContent(5, 5, 950, 150);
+    root.add(image1);
 
-    var check = new drawCheckBox(20, 290, 80, 30);
-    root.add(check);
-
-    var com = new drawComboBox(130, 350, 100, 30);
-    root.add(com);
-
-    var texA = new drawTextArea(20, 300, 180, 100);
-    root.add(texA);
-
-    var lin = new drawHyperlink(350, 20, 150, 20);
-    root.add(lin);
-
-    var me = new drawMenuBar(0, 110, 950, 40);
+    var me = new drawMenuBar(10, 110, 950, 40);
     root.add(me);
 
-  /*  root.properties({
-        layout: new BorderLayout(4, 4),
-        border: new Border(),
-        padding: 8,
-        kids: {
-            CENTER: new BorderPan("UX panel", new Panel({
-                //padding: 50,
-                kids: controls
-            })),
-         
-            BOTTOM: new Button("Align", [
-                function fire() {
-                    this.$super();
-                    var y = 10, c = root.findAll("//zebra.ui.designer.ShaperPan");
-                    for (var i = 0; i < c.length; i++) {
-                        c[i].toPreferredSize();
-                        c[i].setLocation(10, y);
-                        y += c[i].height + 5;
-                    }
-                }
-            ])
-        }
-    });*/
+    var lable1 = new drawLable(720, 183, 80, 30, "User Name");
+    root.add(lable1);
 
+    var text = new drawTextBox(820, 183, 120, 30);
+    root.add(text);
+
+    var lable2 = new drawLable(720, 235, 80, 30, "Password");
+    root.add(lable2);
+
+    var text1 = new drawTextBox(820, 235, 120, 30);
+    root.add(text1);
+
+
+    var hor = new drawHorizontalLine(700, 330, 250);
+    root.add(hor);
+
+    var radio1 = new drawRadioButton(720, 370, 80, 30, "Male");
+    root.add(radio1);
+    var radio2 = new drawRadioButton(720, 400, 80, 30, "Female");
+    root.add(radio2);
+
+    var hor1 = new drawHorizontalLine(700, 450, 250);
+    root.add(hor1);
+
+    var check1 = new drawCheckBox(720, 470, 80, 30, "Java");
+    root.add(check1);
+
+    var check2 = new drawCheckBox(720, 500, 80, 30, "C#");
+    root.add(check2);
+
+    var hor2 = new drawHorizontalLine(700, 570, 250);
+    root.add(hor2);
+
+
+    var com = new drawComboBox(720, 600, 100, 30);
+    root.add(com);
+
+
+    var texA = new drawTextArea(720, 650, 180, 100);
+    root.add(texA);
+
+    var butt = new drawButton(720, 760,60, 30, "submit");
+    root.add(butt);
+
+    var lin = new drawHyperlink(350, 780, 150, 20, "this is a hyperlink");
+    root.add(lin);
+
+    var root = (new zCanvas("designerho", 970, 820)).root;
+    root.setBackground("black");
+
+
+    /*
+  
+  
+       root.properties({
+            layout: new BorderLayout(4, 4),
+            border: new Border(),
+            padding: 8,
+            kids: {
+                CENTER: new BorderPan("UX panel", new Panel({
+                    //padding: 50,
+                    kids: drawButton(60,30,"test")
+                })),
+             
+                BOTTOM: new Button("Align", [
+                    function fire() {
+                        this.$super();
+                        var y = 10, c = root.findAll("//zebra.ui.designer.ShaperPan");
+                        for (var i = 0; i < c.length; i++) {
+                            c[i].toPreferredSize();
+                            c[i].setLocation(10, y);
+                            y += c[i].height + 5;
+                        }
+                    }
+                ])
+            }
+        });
+  
+  
+  
+  */
 
 
 
@@ -225,3 +127,148 @@
 });
 
 
+///////////////////////////editable controlles ///////////////////
+
+
+//function for drawing button
+function drawButton(bX,bY,bwidth, bheight, ButttonCaption) {
+    var button = new zebra.ui.Button(ButttonCaption);
+    button.setBounds(bX, bY, bwidth, bheight);
+   
+
+    var buttonShaperPan = new zebra.ui.designer.ShaperPan(button);
+    
+    return buttonShaperPan;
+
+}
+
+//function for drawing button
+function drawPanel(px, py, pw, ph) {
+    var panel = new zebra.ui.Panel();
+    panel.setBounds(px, py, pw, ph);
+    panel.setBackground("white");
+
+    //var shperPanel = new zebra.ui.designer.ShaperPan(panel);
+
+    return panel;
+
+}
+
+//function for drawing lable
+function drawLable(lx, ly, lw, lh, labaleCaption) {
+    var lable = new zebra.ui.Label(labaleCaption);
+    lable.setBounds(lx, ly, lw, lh);
+    lable.setColor("black");
+
+    var shperlable = new zebra.ui.designer.ShaperPan(lable);
+
+    return shperlable;
+
+}
+
+//function for drawing text box
+function drawTextBox(tx, ty, tw, th) {
+    var textBox = new zebra.ui.TextField("type here...");
+    textBox.setBounds(tx, ty, tw, th);
+
+    var shpertextBox = new zebra.ui.designer.ShaperPan(textBox);
+
+    return shpertextBox;
+}
+
+//function for drawing Horizontal Line
+function drawHorizontalLine(hx, hy, hw) {
+
+    var horizontal = new zebra.ui.Line();
+    horizontal.setBounds(hx, hy, hw, 3);
+    var shperhorizontal = new zebra.ui.designer.ShaperPan(horizontal);
+
+    return shperhorizontal;
+}
+
+//function for drawing checkBox
+function drawCheckBox(checkX, checkY, checkW, checkH, checkValue) {
+
+    var checkBox = new zebra.ui.Checkbox(checkValue);
+    checkBox.setBounds(checkX, checkY, checkW, checkH);
+    var shpercheckBox = new zebra.ui.designer.ShaperPan(checkBox);
+
+    return shpercheckBox;
+}
+
+//function for drawing radio button
+function drawRadioButton(radioX, radioY, radioW, radioH, radioValue) {
+    var radioButton = new zebra.ui.Radiobox(radioValue);
+    radioButton.setBounds(radioX, radioY, radioW, radioH);
+
+    var shpercheckBox = new zebra.ui.designer.ShaperPan(radioButton);
+
+    return shpercheckBox;
+}
+
+//function for drawing combo box
+function drawComboBox(comboX, comboY, comboW, comboH) {
+    var combo = new zebra.ui.Combo(new zebra.ui.List([
+   "Item 1",
+   "Item 2",
+   "Item 3"
+    ]));
+
+    combo.setBounds(comboX, comboY, comboW, comboH);
+    var shpercombo = new zebra.ui.designer.ShaperPan(combo);
+
+    return shpercombo;
+}
+
+//function for drawing TextArea
+function drawTextArea(textAreaX, textAreaY, textAreaW, textAreaH) {
+    var textArea = new zebra.ui.TextArea();
+    textArea.setBounds(textAreaX, textAreaY, textAreaW, textAreaH);
+    var shpertextArea = new zebra.ui.designer.ShaperPan(textArea);
+
+    return shpertextArea;
+}
+
+//function for drawing combo box
+function drawHyperlink(hyX, hyY, hyW, hyH, hyperValue) {
+    var hyperlink = new zebra.ui.Link(hyperValue);
+    hyperlink.setBounds(hyX, hyY, hyW, hyH);
+    hyperlink.setColor("#2E64FE");
+    var shperhyperlink = new zebra.ui.designer.ShaperPan(hyperlink);
+
+    return shperhyperlink;
+}
+
+//function for drawing Menubar
+function drawMenuBar(menuX, menuY, menuW, menuH) {
+    var menu = new zebra.ui.Menubar({
+        "HOME": {
+            "Subitem 1.1": null,
+            "Subitem 1.2": null,
+            "Subitem 1.3": null
+        },
+        "CONTENT": {
+            "Subitem 2.1": null,
+            "Subitem 2.2": null,
+            "Subitem 2.3": null
+        },
+        "CONTCT": null
+    });
+
+    menu.setBounds(menuX, menuY, menuW, menuH);
+    var shpermenu = new zebra.ui.designer.ShaperPan(menu);
+
+
+    return shpermenu;
+}
+
+//function for drawing image
+function drawImageContent(imageX, imageY, imageW, imageH) {
+    var image = new zebra.ui.ImagePan();
+
+    image.setBounds(imageX, imageY, imageW, imageH);
+    var shperimage = new zebra.ui.designer.ShaperPan(image);
+
+
+    return shperimage;
+}
